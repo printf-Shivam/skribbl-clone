ab8936d68be7d64430292dc8e12b1174729c0766
printf-Shivam <shivamkrsingh264@gmail.com>
2025-05-07 11:13:31 +0530

    Update gameController.js
    round end handled

--- server/socket/gameController.js
+++ server/socket/gameController.js
@@ -60,3 +60,47 @@ export function startRoundTimer(io, roomId, rooms){
   }
   },1000);
 }
+
+//sending time left for round to end
+export function getRoundTimeLeft(roomId, rooms) {
+  return Math.floor(ROUND_TIME / 2); 
+}
+
+export function endRound(io, roomId, rooms){
+  const room = rooms[roomId];
+  if(!room) return ;
+
+  clearInterval(room.timer);
+
+  //after the round ends, reveal word to all players 
+  io.to(roomId).emit('roundEnded',{ 
+    word: room.currentWord,
+    scores: room.players.map(p=>({id: p.id, username:p.username, score:p.score}))
+  });
+  
+//check if the game is over 
+  if(room.currentRound >= room.totalRounds* room.players.length) {
+    // Game over
+    setTimeout(()=>{
+      io.to(roomId).emit('gameOver',{
+        players: room.players.sort((a, b) => b.score - a.score)
+      });
+      
+      //reseting the game state 
+      room.currentDrawer= null;
+      room.currentWord= null;
+      room.currentRound= 0;
+      room.guessedPlayers= [];
+      room.gameStarted= false;
+      room.players.forEach(p=>p.score= 0);
+    },3000);
+  }else{
+    //move to the next round if game is not ever
+    room.currentRound++;
+    
+    //start next turn 
+    setTimeout(()=>{
+      startTurn(io,roomId,rooms);
+    },3000);
+  }
+}
